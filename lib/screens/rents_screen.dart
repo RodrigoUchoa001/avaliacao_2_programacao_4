041d45e5d7d03b5163f13f2f0021924a7047ad0e
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-08-04 22:04:02 -0300

    feita organização dos widgets da tela de aluguel

--- lib/screens/rents_screen.dart
+++ lib/screens/rents_screen.dart
@@ -1,5 +1,4 @@
-import 'package:avaliacao_2/widgets/rent_screen/rent_top_selected_button.dart';
-import 'package:avaliacao_2/widgets/top_container.dart';
+import 'package:avaliacao_2/widgets/rent_screen/rent_top_bar_buttons.dart';
 import 'package:flutter/material.dart';
 
 class RentsScreen extends StatelessWidget {
@@ -7,31 +6,12 @@ class RentsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TopContainer(
+    return SingleChildScrollView(
       child: Column(
-        children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                for (int i = 0; i < rentTopButtons.length; i++)
-                  RentTopSelectedButton(index: i, text: rentTopButtons[i].text),
-              ],
-            ),
-          ),
+        children: const [
+          RentTopBarButtons(),
         ],
       ),
     );
   }
 }
-
-class RentTopButton {
-  String text;
-  RentTopButton({required this.text});
-}
-
-List<RentTopButton> rentTopButtons = [
-  RentTopButton(text: 'Previous'),
-  RentTopButton(text: 'Current'),
-  RentTopButton(text: 'Reserved'),
-];
---
+++ lib/widgets/rent_screen/rent_top_bar_buttons.dart
@@ -0,0 +1,31 @@
+import 'package:avaliacao_2/widgets/rent_screen/rent_top_selected_button.dart';
+import 'package:avaliacao_2/widgets/top_container.dart';
+import 'package:flutter/material.dart';
+
+class RentTopBarButtons extends StatelessWidget {
+  const RentTopBarButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TopContainer(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          for (int i = 0; i < rentTopButtons.length; i++)
+            RentTopSelectedButton(index: i, text: rentTopButtons[i].text),
+        ],
+      ),
+    );
+  }
+}
+
+class RentTopButton {
+  String text;
+  RentTopButton({required this.text});
+}
+
+List<RentTopButton> rentTopButtons = [
+  RentTopButton(text: 'Previous'),
+  RentTopButton(text: 'Current'),
+  RentTopButton(text: 'Reserved'),
+];
